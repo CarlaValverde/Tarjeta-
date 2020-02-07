fb236ab84a95f5f13e9a535a428016b6efe58cda
JuanAR05 <amadorrubioj@gmail.com>
2020-02-07 10:41:39 +0100

    Final de la tarea 7. close #7

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
+		//FIN DE LA TAREA NUMERO 7
 		ITarjetaMonedero monedero = new Tarjeta("1234567890", "12345678A", 1234, 600);
 		ITarjetaDebito tarjeta = new Tarjeta("1234567890", "12345678A", 5678, 50000);
 	}
